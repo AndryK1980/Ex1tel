8e716453f453a812717a5bab92eb058a38b677eb
AndryK <a.kriganovsky@gmail.com>
2018-09-26 11:02:59 +0300

    create structure proj

---
+++ Index.php
@@ -0,0 +1,5 @@
+<h1>Create Telefon</h1>
+<form action="Controllers/Telefon.php" method="post">
+<p>Your telefon number: <input name="tel" type="text"></p>
+<p><input type='submit' value='Save'></p>
+</form>
