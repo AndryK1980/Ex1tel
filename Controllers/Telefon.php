95116879b5332f1a7d132e713c6333fb74144b45
AndryK <a.kriganovsky@gmail.com>
2018-09-26 11:19:22 +0300

    veryf from tel

--- Controllers/Telefon.php
+++ Controllers/Telefon.php
@@ -1,3 +1,11 @@
 <?php
-echo 1;
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $tel = $_POST['tel'];
+    if (is_numeric($tel)) {
+        echo var_export($tel, true) . " - tel ready", PHP_EOL;
+    } else {
+        echo var_export($tel, true) . " - error tel", PHP_EOL;
+    }
+    //echo ($tel);
+}
 ?>
